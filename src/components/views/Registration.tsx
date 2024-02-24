c156b484a26c16b9435f3cbfa3cfce0fbf0455bd
Gianluca Nardone <gianlucanardone@macbook-air-von-gigi-und-juju.home>
2024-02-24 15:56:08 +0100

    Created a Registration page

--- src/components/views/Registration.tsx
+++ src/components/views/Registration.tsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 import { api, handleError } from "helpers/api";
 import User from "models/User";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
-import "styles/views/Login.scss";
+import "styles/views/Registration.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 
-/*
-It is possible to add multiple components inside a single file,
-however be sure not to clutter your files with an endless amount!
-As a rule of thumb, use one file per component and only add small,
-specific components that belong to the main one in the same file.
- */
 const FormField = (props) => {
   return (
-    <div className="login field">
+    <div className="registration field">
       <input
-        className="login input"
+        className="registration input"
         placeholder={props.placeholder}
         value={props.value}
         onChange={(e) => props.onChange(e.target.value)}
@@ -25,46 +19,41 @@ const FormField = (props) => {
     </div>
   );
 };
-
+  
 FormField.propTypes = {
   placeholder: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
 };
 
-const Login = () => {
+const Registration = () => {
   const navigate = useNavigate();
   const [name, setName] = useState<string>(null);
   const [username, setUsername] = useState<string>(null);
 
-  const doLogin = async () => {
+  const doRegistration = async () => {
     try {
       const requestBody = JSON.stringify({ username, name });
       const response = await api.post("/users", requestBody);
 
-      // Get the returned user and update a new object.
       const user = new User(response.data);
-
-      // Store the token into the local storage.
       localStorage.setItem("token", user.token);
-
-      // Login successfully worked --> navigate to the route /game in the GameRouter
+      
+      // Redirect the user to the main page after successful registration
       navigate("/game");
     } catch (error) {
-      alert(
-        `Something went wrong during the login: \n${handleError(error)}`
-      );
+      alert(`Something went wrong during the registration: \n${handleError(error)}`);
     }
   };
 
-  const doRegistration = () => {
-    navigate("/registration");
+  const doLogin = () => {
+    navigate("/login");
   } 
 
   return (
     <BaseContainer>
-      <div className="login container">
-        <div className="login form">
+      <div className="registration container">
+        <div className="registration form">
           <FormField
             placeholder="Username"
             value={username}
@@ -75,20 +64,20 @@ const Login = () => {
             value={name}
             onChange={(n) => setName(n)}
           />
-          <div className="login button-container">
+          <div className="registration button-container">
             <Button
               disabled={!username || !name}
               width="100%"
-              onClick={() => doLogin()}
+              onClick={doRegistration()}
             >
-              Login
+              Create Account
             </Button>
             <Button
               width="100%"
-              onClick={() => doRegistration()}
+              onClick={() => doLogin()}
               className="secondary-button"
             >
-              Registration
+              Login
             </Button>
           </div>
         </div>
@@ -97,7 +86,4 @@ const Login = () => {
   );
 };
 
-/**
- * You can get access to the history object's properties via the useLocation, useNavigate, useParams, ... hooks.
- */
-export default Login;
+export default Registration;
